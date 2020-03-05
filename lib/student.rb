c9071d669ae96eb1990a4e85c1d1a45f269f3438
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-05 21:37:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student| 
-      Student.new(students_array)
+      self.new(student)
     end
   end
 
