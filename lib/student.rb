925080179fd0776bcef1eb32011f17ad359733e8
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-05 21:35:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student| 
-      Student.new()
+      Student.new(Scraper.)
   end
 
   def add_student_attributes(attributes_hash)
